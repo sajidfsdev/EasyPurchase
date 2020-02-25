2a7a60b2acf9aee313e89b84eea9f1f9df0dc1bb
Sajid Ali <sajid_ali@elixir.com>
2020-02-25 12:15:04 +0500

    Renovation Started

--- src/App.js
+++ src/App.js
@@ -1,76 +1,13 @@
-import React,{ useEffect } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import * as AuthActions from './Store/Action/Auth';
+import React from "react";
 
-//components starts....
-import Spinner from './Reusable/Bufferring/Bufferring';
-
-//pages imports......
-import LoginPage from './Pages/Login/Login';
-import Grid from './Grid/Grid';
-import Bufferring from './Reusable/Bufferring/Bufferring';
-
-const App=(props)=>{
-
-  //redux state starts here......
-  const auth_RP=useSelector(state=>state.auth.isAuth);
-  const app_buff_RP=useSelector(state=>state.app.bufferring);
-  const resolved_RP=useSelector(state=>state.auth.resolved);
-  const dispatch=useDispatch();
-  //redux state ends here........
-
- 
-  //use effect starts here......
-  useEffect(()=>{
-      dispatch(AuthActions.handleAuthChecking());
-  },[]);
-  //use effect ends here........
-
-
-
-  //GUI man starts here............
-  let mainGUI=null;
-
-  if(resolved_RP)
-  {
-      if(auth_RP)
-      {
-        mainGUI=(<Grid />);
-      }
-      else
-      {
-        mainGUI=(<LoginPage />);
-      }
-  }
-  else
-  {
-    mainGUI=(<Bufferring />);
-  }
-  //GUI man ends here..............
-
-
-  //Gen Bufferring starts here.....
-  let buff=null;
-  if(app_buff_RP)
-  {
-    buff=(<Spinner />);
-  }
-  //Gen bufferring ends............
-
-  //return statement starts here......
+const App = props => {
+  //return starts..
   return (
     <React.Fragment>
-      {
-        buff
-      }
-           <BrowserRouter>
-              {mainGUI}
-           </BrowserRouter>
+      <div>App Under Construction...</div>
     </React.Fragment>
   );
-  //return statement ends here........
-
-}//.....................
+  //return ends....
+}; //................
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 //Redux imports starts here......
-import { createStore,combineReducers,applyMiddleware,compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-import AuthReducer from './Store/Reducer/Auth';
-import AppReducer from './Store/Reducer/App';
+import AuthReducer from "./Store/Reducer/Auth";
+import AppReducer from "./Store/Reducer/App";
 //Redux imports ends here........
 
-
 //Reux management starts here.....
-const rootReducer=combineReducers({
-    auth:AuthReducer,
-    app:AppReducer
-})
+const rootReducer = combineReducers({
+  auth: AuthReducer,
+  app: AppReducer
+});
 
-const store=createStore(rootReducer,compose(applyMiddleware(thunk)));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 //Redux management ends here......
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
