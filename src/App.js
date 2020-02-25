91ee1e8c0a175ea25c4e34cebfecbce82a3475a3
Sajid Ali <sajid_ali@elixir.com>
2020-02-25 12:29:20 +0500

    Changes Revert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Box from "@material-ui/core/box";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const App = props => {
   //return starts..
   return (
     <React.Fragment>
-      <div>App Under Construction...</div>
+      <Container maxWidth="100%">
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        >
+          <Box>Some Compo</Box>
+          <Box>Some Compo</Box>
+        </Typography>
+      </Container>
     </React.Fragment>
   );
   //return ends....
